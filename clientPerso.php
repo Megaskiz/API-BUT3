a3607874af01b76f273cdfa015dc4744c5fbd4de
Adam Mende <*adammende7@gmail.com>
2023-03-16 10:36:39 +0100

    ligne inutile

--- clientPerso.php
+++ clientPerso.php
@@ -5,15 +5,6 @@
 </head>
 <h1>Fun Fact Chuck Norris</h1>
 <?php
-//récupaération du token avec la fonction tokenRequest
-if (isset($_GET['login']) && isset($_GET['password'])) {
-    $login = $_GET['login'];
-    $password = $_GET['password'];
-    $token = tokenRequest($login, $password);
-    if ($token == null) {
-        header('Location: login.php');
-    }
-}
 
 //requete d'ajout de fact
 if (isset($_GET['phrase'])) {
@@ -68,30 +59,3 @@ $json = json_decode($data, true);
     <?php } ?>
 </table>
 
-
-<?php
-
-function tokenRequest($login, $password)
-{
-    $data = array("login" => $login, "password" => $password);
-    $data_string = json_encode($data);
-    $result = file_get_contents(
-        'http://localhost:8080/API_REST/REST/Perso/serveurAuthentication.php',
-        false,
-        stream_context_create(array(
-            'http' => array(
-                'method' => 'POST',
-                'content' => $data_string,
-                'header' => array('Content-Type: application/json' . "\r\n"
-                    . 'Content-Length: ' . strlen($data_string) . "\r\n")
-            )
-        ))
-    );
-
-    //stocker le token dans une variable token
-    $json = json_decode($result, true);
-    $token = $json['data'];
-
-    return $token;
-}
-?>
