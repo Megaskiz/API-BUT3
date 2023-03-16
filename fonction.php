5badc75072280bd26afc4266b3fc7255ea42ab15
jonathan.bzr <jonathan.bezara@icloud.com>
2023-03-16 11:15:48 +0100

    fonction connection terminé

--- fonction.php
+++ fonction.php
@@ -1,15 +1,11 @@
 <?php
-function connecter_bd() {
+function connecter_bd()
+{
     try {
         // Création de la connexion PDO à la base de données
-        $conn = new PDO("mysql:host=localhost; dbname=projetapi", 'root', '');
-
-        // Configuration de PDO pour lever des exceptions en cas d'erreur SQL
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        return $conn; // retourne l'objet de connexion PDO
-    }
-    catch(PDOException $e) {
+        $conn = new PDO("mysql:host=localhost; dbname=projetapi", "root", "");
+        return $conn;
+    } catch (PDOException $e) {
         die("Erreur de connexion à la base de données : " . $e->getMessage());
     }
 }
